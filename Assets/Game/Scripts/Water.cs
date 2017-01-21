062e9a77054202cb3c6434538c3d8b8bc942becf
jaraki <arakij@uci.edu>
2017-01-21 15:03:51 -0800

    Added in basic water animation

---
+++ Assets/Game/Scripts/Water.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+    SpriteRenderer sr;
+    float t;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t += Time.deltaTime;
+        sr.color = new Color(1.0f, 1.0f, 1.0f, 0.5f + 0.5f * (Mathf.Sin(t)));
+	}
+}
