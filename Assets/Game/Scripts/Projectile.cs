d122a5c0be771099a932007ae113cd6598154e8a
jaraki <arakij@uci.edu>
2017-01-21 13:44:48 -0800

    Going to try to push

---
+++ Assets/Game/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public Vector3 direction;
+    public float speed = 5.0f;
+    Rigidbody2D rb;
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = direction.normalized * speed;
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (!player.alive) {
+                player.DisableControls(3.0f);
+            }
+        }
+        Destroy(gameObject);
+    }
+}
