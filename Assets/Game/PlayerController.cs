a785bd6de7f800f9c7b5c1f1873e97c6f89ad593
jfc3 <buckslice@gmail.com>
2017-01-20 22:47:25 -0800

    players start as humans and become zombies when attacked

--- Assets/Game/PlayerController.cs
+++ Assets/Game/PlayerController.cs
@@ -5,40 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed = 2.0f;
-    public bool alive = true;
+    public bool alive = true;   // zombies are dead
     public float health = 100.0f;
     public float dps = 10.0f; // damage that the zombies do to humans per second
+
+    public Sprite playerSprite;
+    public Sprite zombieSprite;
 
     private TopDownGamePad gamepad;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         gamepad = GetComponent<TopDownGamePad>();
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponentInChildren<SpriteRenderer>();
 
         gamepad.OnDisconnect += Remove;
         gamepad.OnColorChanged += ColorChanged;
-	}
-	
-	// Update is called once per frame
+    }
+
+    // Update is called once per frame
     void Update() {
-        if(health <= 0) {
+        if (health <= 0) {
             health = 0.0f;
             alive = false;
+            sr.sprite = zombieSprite;
         }
     }
 
-	void FixedUpdate () {
+    void FixedUpdate() {
         if (gamepad.touching) {
             rb.velocity = gamepad.dir * moveSpeed;
         } else {
             rb.velocity = Vector2.zero;
         }
         //Debug.Log(gamepad.dir);    
-	}
+    }
 
     void ColorChanged(Color c) {
         sr.color = c;
@@ -53,7 +57,7 @@ public class PlayerController : MonoBehaviour {
             PlayerController otherPlayer = collision.gameObject.GetComponent<PlayerController>();
             if (otherPlayer.alive) {
                 otherPlayer.health -= dps * Time.deltaTime;
-                Debug.Log(otherPlayer.health);
+                //Debug.Log(otherPlayer.health);
             }
         }
     }
