20e6715b2830a2a387b90ff7e344bc5bc14a2912
jaraki <arakij@uci.edu>
2017-01-22 12:40:38 -0800

    Added medpack glow when ready

--- Assets/Game/Scripts/MedPack.cs
+++ Assets/Game/Scripts/MedPack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MedPack : MonoBehaviour {
     BoxCollider2D bc;
+    public GameObject glowCircle;
     public float delay = 3.0f;
 	// Use this for initialization
 	void Start () {
@@ -14,5 +15,6 @@ public class MedPack : MonoBehaviour {
     IEnumerator Spawn() {
         yield return new WaitForSeconds(delay);
         bc.enabled = true;
+        glowCircle.SetActive(true);
     }
 }
